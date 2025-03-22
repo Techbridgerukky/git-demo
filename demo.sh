c178e92994a9e8a8a87eba2d70fa41a2a660dd66
Rukayat Salami <rukayat.salami@lightcurve.io>
2025-03-22 14:11:26 +0000

    update demo

---
+++ demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Print a Hello message
+echo "Hello, World!"
+
+# Show the current date and time
+echo "Current Date and Time: $(date)"
+
+# List files in the current directory
+echo "Files in the current directory:"
+ls
